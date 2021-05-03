5ab4e3df6fe0359cb6e05b1e691bb96d79299b12
SebastianVeloza <fernando7829@hotmail.com>
2021-05-03 14:48:55 -0500

    ya se comunican los sockets

--- client.js
+++ client.js
@@ -2,7 +2,10 @@ const { Socket } = require ('net');
 const socket = new Socket();
 
 socket.connect({
-    host: 'localhost', port: 8000
+    host: 'localhost', port: 8000});
+socket.write("Hola");
+socket.setEncoding('utf-8');
+socket.on("data",(data) => {
+    console.log(data);
 
 });
-socket.write('Hola');
