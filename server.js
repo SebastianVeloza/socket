9eab8398065780f7a8d3af5907a33e4824afe88c
SebastianVeloza <fernando7829@hotmail.com>
2021-05-03 14:37:28 -0500

    Ahora puedo leer la data del cliente en el servidor

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const { Socket } = require('dgram');
+
 const { Server } = require('net');
 const server = new Server();
 
 server.on("connection", (Socket) =>{
-    console.log('Nueva conexion de: ', Socket.remoteAddress );
-} )
+    console.log(`Nueva conexion de: ${Socket.remoteAddress}:${Socket.remotePort}`);
+    Socket.on("data",(data) => {
+        console.log(data);
+    });
+} );
 
 server.listen({
     port: 8000, host: '0.0.0.0'
 },() => {
     console.log("escuchando por el puerto 8000");
-})
+});
